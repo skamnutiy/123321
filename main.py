27da0813262847ecb6d155c852ab9dd24cec3f25
.skam <asdfasdf@asdasfd.asdfa>
2025-01-29 09:32:17 +0200

    v2

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from tkinter import *
 SIZE = 400
 GRID_LEN = 4
 GRID_PADDING = 10
-BACKGROUND_COLOR = ""
-BACKGROUND_COLOR_CELL_EMPTY = ""
+BACKGROUND_COLOR = "#bbada0"
+BACKGROUND_COLOR_CELL_EMPTY = "#cdc1b4"
+BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179", 16: "#f59563", 32: "#f67c5f",
+                         64: "#f65e3b", 128: "#edcf72", 256: "#edcc61", 512: "#edc850",
+                         1024: "#edc53f", 2048: "#edc22e"}
 
